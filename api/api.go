27fb9e8c68a235c27d2fdab9a65a618fe996e606
Pulak Kanti Bhowmick <pulak@appscode.com>
2021-02-05 12:08:58 +0600

    Add go-client for creating stateful set

--- api/api.go
+++ api/api.go
@@ -6,6 +6,8 @@ import (
 	"fmt"
 	"path/filepath"
 
+	"k8s.io/apimachinery/pkg/api/resource"
+
 	appsv1 "k8s.io/api/apps/v1"
 	apiv1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -18,6 +20,14 @@ import (
 
 func int32Ptr(i int32) *int32 {
 	return &i
+}
+
+func int64Ptr(i int64) *int64 {
+	return &i
+}
+
+func strPtr(s string) *string {
+	return &s
 }
 
 func CreateClientSet() kubernetes.Interface {
@@ -92,4 +102,71 @@ func CreateDeployment() {
 
 func CreateStatefulSet() {
 	fmt.Println("Create statefulset cmd is ok")
+	clientset := CreateClientSet()
+	stsClient := clientset.AppsV1().StatefulSets(apiv1.NamespaceDefault)
+	statefulSet := &appsv1.StatefulSet{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "mongo-sts",
+		},
+		Spec: appsv1.StatefulSetSpec{
+			Replicas: int32Ptr(3),
+			Selector: &metav1.LabelSelector{
+				MatchLabels: map[string]string{
+					"app": "mongodb",
+				},
+			},
+			Template: apiv1.PodTemplateSpec{
+				ObjectMeta: metav1.ObjectMeta{
+					Labels: map[string]string{
+						"app": "mongodb",
+					},
+				},
+				Spec: apiv1.PodSpec{
+					TerminationGracePeriodSeconds: int64Ptr(10),
+					Containers: []apiv1.Container{
+						{
+							Name:  "mongo",
+							Image: "mongo",
+							Ports: []apiv1.ContainerPort{
+								{
+									Name:          "db-port",
+									ContainerPort: 27017,
+								},
+							},
+							VolumeMounts: []apiv1.VolumeMount{
+								{
+									Name:      "mongo-vol",
+									MountPath: "/db/data",
+								},
+							},
+						},
+					},
+				},
+			},
+			VolumeClaimTemplates: []apiv1.PersistentVolumeClaim{
+				{
+					ObjectMeta: metav1.ObjectMeta{
+						Name: "mongo-vol",
+					},
+					Spec: apiv1.PersistentVolumeClaimSpec{
+						AccessModes:      []apiv1.PersistentVolumeAccessMode{apiv1.ReadWriteOnce},
+						StorageClassName: strPtr("standard"),
+						Resources: apiv1.ResourceRequirements{
+							Requests: apiv1.ResourceList{
+								apiv1.ResourceStorage: resource.MustParse("1Gi"),
+							},
+						},
+					},
+				},
+			},
+			ServiceName: "mongodb-service",
+		},
+	}
+
+	fmt.Println("Creating StatefulSet...")
+	result, err := stsClient.Create(context.TODO(), statefulSet, metav1.CreateOptions{})
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Created StatefulSet: %q\n", result.GetObjectMeta().GetName())
 }
