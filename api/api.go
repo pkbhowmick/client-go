47074451120bd616169d6f98932f825fb53cd41c
Pulak Kanti Bhowmick <pulak@appscode.com>
2021-02-05 19:06:28 +0600

    Add go client for creating job

--- api/api.go
+++ api/api.go
@@ -9,6 +9,7 @@ import (
 	"k8s.io/apimachinery/pkg/api/resource"
 
 	appsv1 "k8s.io/api/apps/v1"
+	batchv1 "k8s.io/api/batch/v1"
 	apiv1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
@@ -294,4 +295,31 @@ func CreateDaemonSet() {
 
 func CreateJob() {
 	fmt.Println("Creating Job . . .")
+	clientset := CreateClientSet()
+	jobClient := clientset.BatchV1().Jobs(apiv1.NamespaceDefault)
+	job := &batchv1.Job{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "job-test",
+		},
+		Spec: batchv1.JobSpec{
+			Template: apiv1.PodTemplateSpec{
+				Spec: apiv1.PodSpec{
+					Containers: []apiv1.Container{
+						{
+							Name:    "go-version",
+							Image:   "golang:alpine",
+							Command: []string{"go", "version"},
+						},
+					},
+					RestartPolicy: apiv1.RestartPolicyNever,
+				},
+			},
+			BackoffLimit: int32Ptr(2),
+		},
+	}
+	result, err := jobClient.Create(context.TODO(), job, metav1.CreateOptions{})
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Created Job %q\n", result.GetObjectMeta().GetName())
 }
